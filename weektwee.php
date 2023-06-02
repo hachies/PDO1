a06a1f5945aea1e02b83a9278c12c0b069132caa
hachies <133209891+hachies@users.noreply.github.com>
2023-06-02 14:46:23 +0200

    Add files via upload

--- weektwee.php
+++ weektwee.php
@@ -1,54 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 <?php
+$host = 'localhost:3307';
+$db   = 'winkel';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
 
-
-$variabele1 = 10;
-$variabele2 = 10;
-
-if ($variabele1 == $variabele2) {
-    echo "De twee waarden zijn gelijk";
-} else {
-    echo "De twee waarden zijn ongelijk";
-}
-
-$variabele1 = 10;
-$variabele2 = 12;
-
-if ($variabele1 != $variabele2) {
-    echo "De twee waarden zijn ongelijk";
-}
-
-$variabele1 = 10;
-$variabele2 = 10;
-
-if ($variabele1 == $variabele2) {
-    echo "De twee waarden zijn gelijk";
-} else {
-    echo "De twee waarden zijn ongelijk";
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try 
+{
+     $pdo = new PDO($dsn, $user, $pass, $options);
+     echo "Connectie gemaakt!";
+} 
+catch (\PDOException $e) 
+{
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 ?>
-
-
-<form method="post" action="weektwee">
-    
-</form>
-<?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    echo "Ingevoerde gebruikersnaam: " . $username . "<br>";
-    echo "Ingevoerd wachtwoord: " . $password;
-}
-?>
-    
-</body>
-</html>
